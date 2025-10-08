95003b9b2b578f28a0d625b512c6cd8aabf2b4a4
fallenVIXA <86466817+fallenVIXA@users.noreply.github.com>
2025-10-09 01:56:25 +0200

    script js n css update

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navLinks.classList.toggle("show");
+  });
+});
+
